b191ed805e3f938e6b7539c9529ae5bdda1029fd
AJTECH0001 <aladejamiudamilola@gmail.com>
2025-07-19 11:32:20 +0100

    add colour dependencies

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,16 @@ use rand::Rng; // Importing the Rng trait from the rand crate
 
 use std::cmp::Ordering; // Importing the Ordering enum for comparison
 
+use colored::*;
+
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-   // println!("The secret number is: {secret_number}");
+
+    // println!("The secret number is: {secret_number}");
+    println!("The secret number is: {}", secret_number);
+
     loop {
         println!("Please input your guess.");
 
@@ -18,19 +23,17 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-         let guess: u32 = match guess.trim().parse() {
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        println!("You guessed: {guess}");
-
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("{}", "Too small!".red()),
+            Ordering::Greater => println!(" {} ", "Too big!".red()),
             Ordering::Equal => {
-                println!("You win!");
-                break; // Exit the loop if the guess is correct
+                println!("{}", "Yow win!".green());
+                break;
             }
         }
     }
